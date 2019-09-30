6c431c98631dd925c5893f658ed010f9e150c920
chrizlm <42278574+chrizlm@users.noreply.github.com>
2019-09-30 15:36:55 +0300

    Add files via upload

---
+++ ThreadDemo/src/lesson1b/Demo.java
@@ -0,0 +1,28 @@
+package lesson1b;
+
+class MyClass extends Thread {
+	
+	public void run() {
+		for(int i=0; i<10; i++) {
+			System.out.println(Thread.currentThread().getId() + "value " +i);
+		}
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+public class Demo {
+
+	public static void main(String[] args) {
+		MyClass myClass = new MyClass();
+		myClass.start();
+		MyClass myClass1 = new MyClass();
+		myClass1.start();
+
+	}
+
+}
